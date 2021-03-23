89d4f493236db99a50238e98037eecf8ffb6df0f
r1s4 <r1s4.po@gmail.com>
2021-03-23 14:41:35 +0900

    initial commit

--- app/src/main/java/com/example/namscom/labm/CalculationActivity.kt
+++ app/src/main/java/com/example/namscom/labm/CalculationActivity.kt
@@ -1,21 +1,252 @@
 package com.example.namscom.labm
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_calculation.*
 
 class CalculationActivity : AppCompatActivity() {
+    lateinit var mUserDao: UserDao
+    lateinit var mAdapter: ArrayAdapter<String>
+
+    private var mUserList: List<User> = listOf()
+
+    var nStr : String = ""
+    val nList = ArrayList<Double>() // arraylist to store numbers
+    val oList = ArrayList<Char>() // arraylist to store operations
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculation)
 
+        mUserDao = UserDatabase.getInstance(this).userDao()
+
+        mAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayListOf())
+
+        getUser()
+
+        num0.setOnClickListener {
+            formula.text = "${formula.text}0"
+            nStr += "0"
+        }
+        num1.setOnClickListener {
+            formula.text = "${formula.text}1"
+            nStr += "1"
+        }
+        num2.setOnClickListener {
+            formula.text = "${formula.text}2"
+            nStr += "2"
+        }
+        num3.setOnClickListener {
+            formula.text = "${formula.text}3"
+            nStr += "3"
+        }
+        num4.setOnClickListener {
+            formula.text = "${formula.text}4"
+            nStr += "4"
+        }
+        num5.setOnClickListener {
+            formula.text = "${formula.text}5"
+            nStr += "5"
+        }
+        num6.setOnClickListener {
+            formula.text = "${formula.text}6"
+            nStr += "6"
+        }
+        num7.setOnClickListener {
+            formula.text = "${formula.text}7"
+            nStr += "7"
+        }
+        num8.setOnClickListener {
+            formula.text = "${formula.text}8"
+            nStr += "8"
+        }
+        num9.setOnClickListener {
+            formula.text = "${formula.text}9"
+            nStr += "9"
+        }
+        point.setOnClickListener {
+            formula.text = "${formula.text}."
+            nStr += "."
+        }
+        equal.setOnClickListener {
+            formula.text = "${formula.text}="
+            addList(nStr)
+            var result = calcualte().toString()
+            formula.text = result
+            nStr = result
+            nList.clear()
+            oList.clear()
+        }
+        add.setOnClickListener {
+            formula.text = "${formula.text}+"
+            addList(nStr,'+')
+            nStr = ""
+        }
+        subtract.setOnClickListener {
+            formula.text = "${formula.text}-"
+            addList(nStr,'-')
+            nStr = ""
+        }
+        multiply.setOnClickListener {
+            formula.text = "${formula.text}*"
+            addList(nStr,'*')
+            nStr = ""
+        }
+        divide.setOnClickListener {
+            formula.text = "${formula.text}/"
+            addList(nStr,'/')
+            nStr = ""
+        }
+        delete.setOnClickListener {
+            var formulaStr = formula.text.toString()
+            if (!formulaStr.isEmpty()) {
+                formula.text = formulaStr.subSequence(0,formulaStr.lastIndex)
+            }
+            if (!nStr.isEmpty()) {
+                nStr = nStr.substring(0, nStr.lastIndex)
+            }
+        }
+        percent.setOnClickListener {
+            formula.text = "${formula.text}%"
+        }
+        sign.setOnClickListener {
+
+        }
+        clear.setOnClickListener {
+            formula.text = ""
+            nStr = ""
+            nList.clear()
+            oList.clear()
+        }
+
+        //String?になってしまう
+        var flag = intent.getStringExtra("MoneyFlag")
+        var uname = intent.getStringExtra("UserName")
+
+        if(flag!=null || uname!=null) {
+            val money_flag=flag.toString()
+            val user_name=uname.toString()
+            //ENTERボタン
+            enter.setOnClickListener {
+                var title_text = ""
+
+
+                //var total = formula.getText()
+                var total = nStr
+                var total_money=0
+
+                if (money_flag == "0") {
+                    title_text = "${total}円の貸し"
+                } else if (money_flag == "1") {
+                    title_text = "${total}円の借り"
+                }
+                AlertDialog.Builder(this) // FragmentではActivityを取得して生成
+                        .setTitle(title_text)
+                        .setMessage("保存しますか？")
+                        .setPositiveButton("保存", { dialog, which ->
+                            // データを保存してホームに戻る
+                            if (money_flag == "0") {
+                                //貸す側なのでDBではプラス
+                                total_money = total.toInt() + mUserDao.getFromNAME(user_name).total
+                                //updateUser(user_name, total.toInt())
+                            }else if(money_flag =="1"){
+                                //借りる側なのでDB上ではマイナス
+                                total_money = -(total.toInt()) +mUserDao.getFromNAME(user_name).total
+                            }
+
+                            println(total_money)
+
+                            // HOMEページへ画面遷移
+                            val intentsub = Intent(this, MainActivity::class.java)
+                            intentsub.putExtra("TotalMoney", total_money.toString())
+                            intentsub.putExtra("UserName",user_name)
+                            setResult(Activity.RESULT_OK,intentsub)
+                            finish()
+                        })
+
+
+                        //    val intent = Intent(this, MainActivity::class.java)
+                        //    startActivity(intent)
+                        //})
+                        .setNegativeButton("いいえ", { dialog, which ->
+                            // 何もせずに閉じる
+                        })
+                        .show()
+            }   //end enter.setOnClickListener
+        }
+
+        //HOMEボタン
         btnHome.setOnClickListener {
+            val user_name=uname.toString()
+            var total_money=mUserDao.getFromNAME(user_name).total
             // Homeページへ画面遷移
-            val intent = Intent(this, MainActivity::class.java)
-            //intent.putExtra("VALUE", editText.text.toString())
-            startActivity(intent)
+            val intentsub = Intent(this, MainActivity::class.java)
+            //nullを返さない用
+            intentsub.putExtra("TotalMoney", total_money.toString());
+            setResult(Activity.RESULT_OK,intentsub)
+            finish()
+        }
+
+    } // end fun onCreate
+
+    fun addList(str : String, ope : Char) {
+        try {
+            var num = str.toDouble()
+            nList.add(num)
+            oList.add(ope)
+        }catch(e:Exception){
+            formula.text = "Numeric error"
         }
     }
-}
+
+    fun addList(str : String) {
+        try {
+            var num = str.toDouble()
+            nList.add(num)
+        }catch(e:Exception){
+            formula.text = "Numeric error"
+        }
+    }
+
+    fun calcualte() : Double {
+
+        var i = 0
+        while (i < oList.size) {
+            //do multiplication and division first
+            if(oList.get(i) == '*' || oList.get(i) == '/') {
+                var result = if (oList.get(i) == '*') nList.get(i) * nList.get(i+1) else nList.get(i) / nList.get(i+1)
+                nList.set(i,result)
+                nList.removeAt(i+1)
+                oList.removeAt(i)
+                i--
+            }
+            // change subtraction to addition
+            else if(oList.get(i) == '-'){
+                oList.set(i,'+')
+                nList.set(i+1,nList.get(i+1) * -1)
+            }
+            i++
+        }
+
+        // get sum
+        var result = 0.0
+        for (i in nList){
+            result += i
+        }
+
+        return result
+    }// end fun calcualte
+
+    private fun getUser() {
+        mUserList = mUserDao.getAll()
+        val userInfoList = mUserList.map {it.name}
+        mAdapter.clear()
+        mAdapter.addAll(userInfoList)
+    }
+
+
+} // end class
--- app/src/main/java/com/example/namscom/labm/MainActivity.kt
+++ app/src/main/java/com/example/namscom/labm/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.namscom.labm
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     private var mUserList: List<User> = listOf()
 
+    private val RESULT_SUBACTIVITY = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,25 +32,25 @@ class MainActivity : AppCompatActivity() {
         getUser()
 
         //+ボタンでの処理（ユーザの追加）
-        btnAdd.setOnClickListener{
+        btnAdd.setOnClickListener {
 
             //ダイアログ中のテキストエディタの設定
-            val dialogEditText =  EditText(this)
+            val dialogEditText = EditText(this)
             //文字数を15文字以下に設定
             val lengthFilter = InputFilter.LengthFilter(15)
             dialogEditText.filters = arrayOf(lengthFilter)
 
             AlertDialog.Builder(this)
-                .setTitle("ユーザの追加")
-                .setMessage("名前：")
-                .setView(dialogEditText)
-                .setPositiveButton("OK", { dialog, which ->
-                    val userText = dialogEditText.text.toString()
-                    if (userText != null) {
-                        insertUser(userText)
-                    }
-                })
-                .show()
+                    .setTitle("ユーザの追加")
+                    .setMessage("名前：")
+                    .setView(dialogEditText)
+                    .setPositiveButton("追加", { dialog, which ->
+                        val userText = dialogEditText.text.toString()
+                        if (userText != null) {
+                            insertUser(userText)
+                        }
+                    })
+                    .show()
         }
 
         if (list_view != null) {
@@ -57,18 +60,18 @@ class MainActivity : AppCompatActivity() {
                 val dialog_user_name = parent.getItemAtPosition(position) as String
 
                 //貸すor借りるを計算に引き継ぐ
-                var money_flag=0
+                var money_flag = 0
 
                 //DB上のお金のデータ（プラスなら貸してる，マイナスなら借りてる）
-                var dialog_user_money=mUserDao.getFromNAME(dialog_user_name).total
+                var dialog_user_money = mUserDao.getFromNAME(dialog_user_name).total
 
                 //表示するテキスト（タイトル）の内容
-                var text_message =""
+                var text_message = ""
                 //貸してるか借りてるかで表示が変わる
-                if (dialog_user_money>=0) {
-                    text_message="${dialog_user_name}さんに${dialog_user_money} 円の貸しです"
-                } else if(dialog_user_money<0){
-                    text_message="${dialog_user_name}さんに${-(dialog_user_money)} 円の借りです"
+                if (dialog_user_money >= 0) {
+                    text_message = "${dialog_user_name}さんに${dialog_user_money} 円の貸しです"
+                } else if (dialog_user_money < 0) {
+                    text_message = "${dialog_user_name}さんに${-(dialog_user_money)} 円の借りです"
                 }
 
 
@@ -79,26 +82,49 @@ class MainActivity : AppCompatActivity() {
                         .setTitle(text_message)
                         .setItems(strList, { dialog, which ->
                             //貸す判定（0）か，借りる判定(1)か
-                            if(which==0){
-                                money_flag=0
-                            }else if (which==1){
-                                money_flag=1
+                            if (which == 0) {
+                                money_flag = 0
+                            } else if (which == 1) {
+                                money_flag = 1
                             }
                             // 計算ページへ画面遷移
                             val intent = Intent(this, CalculationActivity::class.java)
-                            //intent.putExtra("VALUE", editText.text.toString())
-                            startActivity(intent)
+                            intent.putExtra("MoneyFlag", money_flag.toString());
+                            intent.putExtra("UserName", dialog_user_name);
+                            startActivityForResult(intent, RESULT_SUBACTIVITY)
 
                         })
+
                         .setNeutralButton("削除", { dialog, which ->
                             //削除
                             deleteUser(dialog_user_name)
                         })
+
                         .setPositiveButton("閉じる", { dialog, which ->
                             // 何もせずに閉じる
                         })
                         .show()
+
+            }   //end list_view
+        }   //end if
+    }   //end inCreate
+
+
+    // 計算結果を受け取って，DBに保存する
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == RESULT_SUBACTIVITY && intent != null) {
+            var total_money_temp = intent.getStringExtra("TotalMoney")
+            var user_name_temp = intent.getStringExtra("UserName")
+            if(total_money_temp!=null || user_name_temp!=null) {
+                val user_name = user_name_temp.toString()
+                val total_money = total_money_temp.toString()
+                println(user_name)
+                println(total_money)
+                updateUser(user_name, total_money.toInt())
             }
+
         }
     }
 
@@ -117,6 +143,19 @@ class MainActivity : AppCompatActivity() {
         getUser()
     }
 
+    //更新
+    private fun updateUser(username:String,usertotal:Int){
+        val user=mUserDao.getFromNAME(username)
+        user.total=usertotal
+        //時間の更新
+        //user.date=
+        //メモの更新
+
+        mUserDao.update(user)
+
+        getUser()
+    }
+
 
     //idから削除　（少しくどいので改良する）
     private fun deleteUser(username: String) {
@@ -124,4 +163,5 @@ class MainActivity : AppCompatActivity() {
         mUserDao.delete(deleteUser)
         getUser()
     }
+
 }
